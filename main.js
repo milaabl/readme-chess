ab090e15c908249f3bf388606400e11687b46b51
Ludmila <milaabl2405@gmail.com>
2023-07-29 19:34:50 +0300

    🌈 Style changes

--- main.js
+++ main.js
@@ -5,11 +5,15 @@ const axios = require('axios');
 (async () => {
     const res = await axios.get("https://raw.githubusercontent.com/milaabl/readme-chess/master/README.md");
 
+    fs.writeFileSync("./node_modules/mdimg/template/css/github.css", `@import "https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.min.css";
+    @import "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown-light.min.css";
+    .markdown-body {
+      padding: 0;
+    }`)
+
     const data = res.data;
 
     const readmeTextContents = data.split('<!-- BEGIN CHESS BOARD -->')[1].split('<!-- END CHESS BOARD -->')[0].replaceAll(`"img`, `"https://raw.githubusercontent.com/milaabl/readme-chess/master/img`);
-
-    console.log({readmeTextContents});
 
 
     fs.writeFileSync('./file.md', readmeTextContents);
@@ -18,8 +22,8 @@ const axios = require('axios');
     await convert2img({
         mdFile: "./file.md",
         outputFilename: "./chess.png",
-        width: 600,
         cssTemplate: "github",
+        width: 500
     });
 
 })();
